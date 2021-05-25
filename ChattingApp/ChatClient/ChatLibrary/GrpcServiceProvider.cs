3eb2656e621ccc96524a8962093b9ab13497f342
tingaialamaxim <tingaialamaxim@yahoo.com>
2021-05-25 21:28:32 +0300

    Added class library ChatLibrary for ChatClient.

---
+++ ChattingApp/ChatClient/ChatLibrary/GrpcServiceProvider.cs
@@ -0,0 +1,21 @@
+﻿using Grpc.Net.Client;
+using ChatProtos;
+using System;
+
+namespace ChatLibrary
+{
+    public class GrpcServiceProvider
+    {
+        private string Url { get; set; }
+        private Lazy<GrpcChannel> RpcChannel { get; set; }
+        private Greeter.GreeterClient ChatClient { get; set; }
+
+        public GrpcServiceProvider()
+        {
+            this.Url = "https://localhost:5001";
+            this.RpcChannel = new Lazy<GrpcChannel>(GrpcChannel.ForAddress(this.Url));
+        }
+
+        public Greeter.GreeterClient GetGreeterClient() => this.ChatClient ??= new Greeter.GreeterClient(this.RpcChannel.Value);
+    }
+}
