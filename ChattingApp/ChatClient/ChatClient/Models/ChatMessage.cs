b52f678e1858dc4fa10d2ef0ff0d46a84a425134
Valent <80772449+Vlad-Valentin@users.noreply.github.com>
2021-05-30 11:53:58 +0300

    ChatMessage + MainWindow changes.
    Added model class ChatMessage in the client side.
    Refractored MainWindow.cs.

---
+++ ChattingApp/ChatClient/ChatClient/Models/ChatMessage.cs
@@ -0,0 +1,19 @@
+﻿namespace ChatClient.Models
+{
+    public class ChatMessage
+    {
+        public string Text { get; set; }
+        public string Sender { get; set; }
+
+        public ChatMessage(string text, string sender)
+        {
+            Text = text;
+            Sender = sender;
+        }
+
+        public override string ToString()
+        {
+            return $"{Sender}: {Text}\n";
+        }
+    }
+}
--- ChattingApp/ChatClient/ChatClient/Views/MainWindow.xaml.cs
+++ ChattingApp/ChatClient/ChatClient/Views/MainWindow.xaml.cs
@@ -1,54 +1,159 @@
-﻿using ChatClient.Utility;
+﻿using ChatClient.Grpc;
 using ChatClient.ViewModels;
-using ChatLibrary;
+using Grpc.Net.Client;
 using System;
 using System.Collections.Generic;
-using System.Collections.ObjectModel;
-using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Input;
 
 namespace ChatClient.Views
 {
     public partial class MainWindow : Window
     {
-        public MainWindow(string username,
-            ObservableCollection<string> users,
-            ObservableCollection<string> messages,
-            UserDetails userDetails)
+        private static List<string> listOfMessages = new();
+        private static GrpcChannel RpcChannel = GrpcChannel.ForAddress("https://localhost:5001");
+        private static MessengerService.MessengerServiceClient ChatClient = new MessengerService.MessengerServiceClient(RpcChannel);
+
+        private ChatClientRpc client;
+
+        public MainWindow(string username)
         {
-           DataContext = new MainWindowVM();
+            //_ = Join(username);
+            DataContext = new MainWindowVM();
 
             InitializeComponent();
 
             UsernameBlock.Text = username;
-            UserBox.ItemsSource = users;
-            MessageBox.ItemsSource = messages;
+            client = new();
+            client.RecievedMessages += OnMessageRecieved;
         }
 
-        private void Send_Click(object sender, RoutedEventArgs e)
+        private void SetOnline()
         {
-            _ = CallGrpcService();
+            //To-DO
         }
 
-        private async Task CallGrpcService()
+        private void SetOffline()
         {
-            /* var client = new GrpcServiceProvider().ge;
-             var reply = await client.SayHelloAsync(
-                               new HelloRequest { Name = "WPF client" });
+            //To-DO
+        }
 
-             this.UserText.Text = reply.Message;*/
-            var rand = new Random();
-            var client = new GrpcServiceProvider().GetMessengerClient();
-            Message chatsv = new Message()
+        private void OnMessageRecieved(object sender, RecievedMessagesEventArgs args)
+        {
+            try
             {
-                Text = UserText.Text,
-                User = new User()
+                Dispatcher.Invoke(new Action(() =>
                 {
-                    Id = rand.Next(0, 10),
-                    Name = UsernameBlock.Text
-                },
-            };
-            var reply = await client.SendMessageAsync(request: new SendRequest { Message = chatsv });
+                    foreach (var msg in args.Messages)
+                    {
+                        MainWindowVM.MessageList.Add(msg.ToString());
+                    }
+                }));
+            }
+            catch (System.Threading.Tasks.TaskCanceledException taskException)
+            {
+                Console.WriteLine(taskException.Message);
+                //continue by shutting down client
+                //or restarting him
+            }
         }
+
+        
+
+        private void Canvas_MouseDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed)
+            {
+                DragMove();
+            }
+
+            if (e.MiddleButton == MouseButtonState.Pressed)
+            {
+                Close();
+            }
+        }
+
+        /* private async Task Join(string username)
+         {
+             Thread recieveMessagesThread = new Thread(new ThreadStart(ServerToClient));
+             recieveMessagesThread.Start();
+             while (recieveMessagesThread.ThreadState != ThreadState.Stopped)
+             {
+                 foreach (var message in listOfMessages)
+                 {
+                     MainWindowVM.MessageList.Add(message);
+                 }
+             }
+             await ChatClient.JoinAsync(new Message { User = new User { Id = 0, Name = username }, Text = " is here" });
+         }
+
+         private void Send_Click(object sender, RoutedEventArgs e)
+         {
+             _ = SendMessage();
+         }
+
+         private async Task SendMessage()
+         {
+             //_ = CallGrpcService();
+             _ = Join("");
+             string msg;
+
+             bool isConnected = true;
+             //while (isConnected)
+             //{
+                 msg = UserText.Text;
+                 if (msg.Equals("EXIT"))
+                 {
+                     isConnected = false;
+                     await ChatClient.LeaveAsync(new Message { User = new User { Id = 0, Name = UsernameBlock.Text }, Text = msg });
+                 }
+                 else
+                 {
+                     await ChatClient.ClientToServerAsync(new Message { User = new User { Id = 0, Name = UsernameBlock.Text }, Text = msg });
+                 }
+             MainWindowVM.MessageList.Add(msg);
+             //}
+         }
+
+         //private async Task CallGrpcService()
+         //{
+         //    /* var client = new GrpcServiceProvider().ge;
+         //     var reply = await client.SayHelloAsync(
+         //                       new HelloRequest { Name = "WPF client" });
+
+         //     this.UserText.Text = reply.Message;*/
+        //    //var rand = new Random();
+        //    //var client = new GrpcServiceProvider().GetMessengerClient();
+        //    //Message chatsv = new Message()
+        //    //{
+        //    //    Text = UserText.Text,
+        //    //    User = new User()
+        //    //    {
+        //    //        Id = rand.Next(0, 10),
+        //    //        Name = UsernameBlock.Text
+        //    //    },
+        //    //};
+        //    //var reply = await client.SendMessageAsync(request: new SendRequest { Message = chatsv });
+        //}
+
+        /* private async void ServerToClient()
+         {
+             await Task.Run(async () =>
+             {
+                 var dataStream = ChatClient.ServerToClient(new Empty());
+
+                 try
+                 {
+                     await foreach (var message in dataStream.ResponseStream.ReadAllAsync())
+                     {
+                         listOfMessages.Add(message.Text);
+                     }
+                 }
+                 catch (RpcException e) when (e.StatusCode == StatusCode.Cancelled)
+                 {
+                     Console.WriteLine("Stream cancelled by client.");
+                 }
+             });
+         }*/
     }
 }
