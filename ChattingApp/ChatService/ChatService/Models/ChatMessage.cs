31e416b22cbdf352d4c51159828ee2ab6ea470b6
TupeaAndrei <59830697+TupeaAndrei@users.noreply.github.com>
2021-05-30 11:11:49 +0300

    Refactored chat.proto and added model class ChatMessage.
    After tests the old proto didn't fit our application and required some modifications along side some cleaning.
    The model class will help whenever the generated Message class will have problems.

---
+++ ChattingApp/ChatService/ChatService/Models/ChatMessage.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ChatService.Models
+{
+    public class ChatMessage
+    {
+        public string Text { get; set; }
+        public string Sender { get; set; }
+
+        public ChatMessage(string text, string sender)
+        {
+            Text = text;
+            Sender = sender;
+        }
+    }
+}
