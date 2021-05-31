fc025f9d6f43de4c150ec8ba2812e032c5a732c3
tingaialamaxim <tingaialamaxim@yahoo.com>
2021-05-31 18:40:39 +0300

    Added methods in ChatClientRpc for userList.

--- ChattingApp/ChatClient/ChatClient/Grpc/ChatClientRpc.cs
+++ ChattingApp/ChatClient/ChatClient/Grpc/ChatClientRpc.cs
@@ -13,25 +13,41 @@ namespace ChatClient.Grpc
     {
         public IEnumerable<ChatMessage> Messages;
     }
+
+    public class RecievedUsersEventArgs:EventArgs
+    {
+        public IEnumerable<string> Users;
+    }
     public class ChatClientRpc
     {
         public const int INTERVAL = 1000;
 
         public bool firstConnection = true;
+        public bool firstUserConnection = true;
 
         public delegate void MessagesRecievedHandler(object Sender, RecievedMessagesEventArgs args);
         public event MessagesRecievedHandler RecievedMessages;
+
+        public delegate void UsersRecievedHandler(object sender, RecievedUsersEventArgs args);
+        public event UsersRecievedHandler RecievedUsers;
+
         private int updating = 0;
+        private int userUpdating = 0;
 
         ChatClient.MessengerService.MessengerServiceClient client = new GrpcServiceProvider().GetMessengerClient();
         Timer timer;
+        Timer userTimer;
 
         IEnumerable<ChatMessage> messages = new List<ChatMessage>();
         IEnumerable<ChatMessage> oldMessages = new List<ChatMessage>();
+        IEnumerable<string> users = new List<string>();
+        IEnumerable<string> oldUsers = new List<string>();
         public ChatClientRpc()
         {
             timer = new Timer(UpdateMessages, null, INTERVAL, -1);
+            userTimer = new Timer(UpdateUsers, null, INTERVAL, -1);
             oldMessages.ToList().Add(new ChatMessage("1", "1"));
+            oldUsers.ToList().Add("1");
         }
 
         private async void UpdateMessages(object state)
@@ -46,13 +62,13 @@ namespace ChatClient.Grpc
                     if (firstConnection)
                     {
                         RecievedMessages?.Invoke(this, new RecievedMessagesEventArgs() { Messages = messages });
-                        firstConnection = false;
                     }
-                    if (messages.Count() != oldMessages.Count())
+                    if (messages.Count() != oldMessages.Count() && !firstConnection)
                     {
 
                         RecievedMessages?.Invoke(this, new RecievedMessagesEventArgs() { Messages = messages.TakeLast(1) });
                     }
+                    firstConnection = false;
                     oldMessages = messages;
                     timer.Change(INTERVAL, -1);
                 }
@@ -63,6 +79,38 @@ namespace ChatClient.Grpc
                 finally
                 {
                     Interlocked.Exchange(ref updating, 0);
+                }
+            }
+        }
+
+        private async void UpdateUsers(object state)
+        {
+            if (Interlocked.CompareExchange(ref userUpdating,1,0) == 0)
+            {
+                try
+                {
+                    users.ToList().Clear();
+                    userTimer.Change(-1, -1);
+                    users = await GetUsersAsync();
+                    if (firstUserConnection)
+                    {
+                        RecievedUsers?.Invoke(this, new RecievedUsersEventArgs() { Users = users });
+                    }
+                    if (users.Count() != oldUsers.Count() && !firstUserConnection)
+                    {
+                        RecievedUsers?.Invoke(this, new RecievedUsersEventArgs() { Users = users.TakeLast(1) });
+                    }
+                    firstUserConnection = false;
+                    oldUsers = users;
+                    userTimer.Change(INTERVAL, -1);
+                }
+                catch(RpcException ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+                finally
+                {
+                    Interlocked.Exchange(ref userUpdating, 0);
                 }
             }
         }
@@ -81,14 +129,38 @@ namespace ChatClient.Grpc
             return messages;
         }
 
+        private async Task<IEnumerable<string>> GetUsersAsync()
+        {
+            List<string> users = new();
+            using (var stream = client.GetUsers(new GetUserRequest()))
+            {
+                while (await stream.ResponseStream.MoveNext())
+                {
+                    var currentUser = stream.ResponseStream.Current;
+                    users.Add(currentUser.Username);
+                }
+            }
+            return users;
+        }
+
         public void Refresh()
         {
             UpdateMessages(null);
         }
 
+        public void RefreshUsers()
+        {
+            UpdateUsers(null);
+        }
+
         public void SendMessage(string from, string text)
         {
             client.SendMessage(new SendRequest { From = from, Text = text });
+        }
+
+        public void SendUser(string username)
+        {
+            client.SendUser(new SendUserRequest { Username = username });
         }
 
         public void ShutDown()
